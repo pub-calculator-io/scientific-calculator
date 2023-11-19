15a5890b148281114d31170533cd22830696de23
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:27:14 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Scientific Calculator by Calculator.iO
+Plugin Name: Scientific Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/scientific-calculator/
 Description: This scientific calculator is a free tool that solves complex mathematical expressions by supporting several built-in functions.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Scientific Cal
 
 function display_ci_scientific_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/scientific-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Scientific Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_scientific_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Scientific Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_scientific_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_scientific_calculator', 'display_ci_scientific_calculator' );
